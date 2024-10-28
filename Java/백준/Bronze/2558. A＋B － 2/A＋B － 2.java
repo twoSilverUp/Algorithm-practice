9e3def6e1de7163a0e280c8b180472b97be4d0a9
twoSilverUp <162277335+twoSilverUp@users.noreply.github.com>
2024-10-28 10:04:20 +0900

    [Bronze V] Title: A+B - 2, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BigInteger a = new BigInteger(br.readLine());
+		BigInteger b = new BigInteger(br.readLine());
+		System.out.println(a.add(b));
+	}
+
+}
