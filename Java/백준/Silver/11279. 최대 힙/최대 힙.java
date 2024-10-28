ed0e5ea9428150fd606cc369858a10ff2fa91d18
twoSilverUp <162277335+twoSilverUp@users.noreply.github.com>
2024-10-28 10:14:03 +0900

    [Silver II] Title: 최대 힙, Time: 276 ms, Memory: 26600 KB -BaekjoonHub

---
+++ Java/백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0; i<n; i++){
+            int input = Integer.parseInt(br.readLine());
+            if(input == 0){
+                if(pQ.isEmpty()) bw.write(0+"\n");
+                else bw.write(pQ.poll()+"\n");
+            }
+            else{
+                pQ.add(input);
+            }
+        }
+        bw.close();
+    }
+}
