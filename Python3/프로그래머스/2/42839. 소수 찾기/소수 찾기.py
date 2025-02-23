a338799b637f590aa50f223846761fb8462743bf
twoSilverUp <162277335+twoSilverUp@users.noreply.github.com>
2025-02-23 22:58:35 +0900

    [level 2] Title: 소수 찾기, Time: 2163.11 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,20 @@
+from itertools import permutations as per
+def solution(numbers):
+    answer = 0
+    candidates = []
+    for i in range(1, len(numbers)+1):
+        p = per(numbers, i)
+        for j in p:
+            candidates.append(int(''.join(map(str,j))))
+    
+    candidates = list(set(candidates))
+    for n in candidates:
+        isPrime = True
+        for i in range(2,n):
+            if n%i==0:
+                isPrime=False
+                break
+        if isPrime and n!=1 and n!=0:
+            answer+=1
+            
+    return answer
